d1cf71decd4e67899090d89a891619dc12f86e29
wesqu <wesquel@hotmail.com>
2022-10-19 15:47:28 -0300

    Adicionado o numero à mesa.

--- database/migrations/2022_10_19_174013_create_mesas_table.php
+++ database/migrations/2022_10_19_174013_create_mesas_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('mesas', function (Blueprint $table) {
             $table->id();
+            $table->integer('numero');
             $table->integer('lugares');
             $table->timestamps();
         });
