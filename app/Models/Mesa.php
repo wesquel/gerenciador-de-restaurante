f189a3a2d195ff2c05fc33457bf5e16e5e2f850f
wesqu <wesquel@hotmail.com>
2022-10-19 15:48:13 -0300

    Relacionando Mesa à Comanda.

--- app/Models/Mesa.php
+++ app/Models/Mesa.php
@@ -11,7 +11,12 @@ class Mesa extends Model
 
     protected $fillable = [
         'id',
+        'numero',
         'lugares',
     ];
+
+    public function comanda(){
+        return $this->hasOne(Comanda::class);
+    }
 
 }
