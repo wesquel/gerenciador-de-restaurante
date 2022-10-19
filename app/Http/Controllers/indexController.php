d68921db3e7a3eb80a8f2bc5b961045b33e05449
wesqu <wesquel@hotmail.com>
2022-10-19 15:49:36 -0300

    Criação do controller index.

---
+++ app/Http/Controllers/indexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mesa;
+use Illuminate\Http\Request;
+
+class indexController extends Controller
+{
+    //
+    public function create(){
+
+        return view('welcome');
+    }
+}
