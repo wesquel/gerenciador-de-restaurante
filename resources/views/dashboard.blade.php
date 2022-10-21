d404387f878354ffbaafaf1beedff4c602a875f8
wesqu <wesquel@hotmail.com>
2022-10-20 21:27:34 -0300

    Dashboard listando as mesas.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,37 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <table class="table container-sm">
+                        <thead>
+                            <tr>
+                                <th>Número</th>
+                                <th>Lugares</th>
+                                <th>Status</th>
+                                <th>Selecione</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($mesas as $mesa)
+                            <tr>
+                                <td>{{$mesa->id}}</td>
+                                <td>{{$mesa->lugares}}</td>
+                                <td class="@if($mesa->status == "Ocupado")text-danger @endif text-success">{{$mesa->status}}</td>
+                                <td><button class="btn btn-success">Selecionar</button></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
+<script>
+    var myModal = document.getElementById('myModal')
+    var myInput = document.getElementById('myInput')
+
+    myModal.addEventListener('shown.bs.modal', function () {
+        myInput.focus()
+    })
+</script>
