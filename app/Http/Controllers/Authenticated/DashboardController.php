dbbe07488b8827b8080ad7f0a6ebf841ac6eeac7
wesqu <wesquel@hotmail.com>
2022-10-23 08:38:13 -0300

    Adicionada função de atualização da quantidade de produtos da comanda.

---
+++ app/Http/Controllers/Authenticated/DashboardController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\authenticated;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comanda;
+use App\Models\Mesa;
+use App\Models\Produtos;
+use App\Models\ProdutosComanda;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+
+    public $mesaAtual;
+
+    public function create(){
+        $mesas = Mesa::all();
+        $produtos = Produtos::all();
+        return view('dashboard', ['mesas' => $mesas, 'produtos' => $produtos]);
+    }
+
+    public function mesaChanged($id){
+        $mesa = Mesa::findOrFail($id);
+        if ($mesa->status == "Ocupado"){
+            $mesa->status = "Disponivel";
+        }else{
+            $mesa->status = "Ocupado";
+            $comanda = Comanda::create([
+                'mesa_id' => $mesa->id,
+                'valor' => 0,
+            ]);
+            event(new Registered($comanda));
+
+            $produtos = Produtos::all();
+            foreach ($produtos as $produto){
+                $produtosComanda = ProdutosComanda::create([
+                    'comanda_id' => $comanda->id,
+                    'quantidade' => 0,
+                    'produto_id' => $produto->id,
+                ]);
+                event(new Registered($produtosComanda));
+            }
+        }
+        $mesa->update();
+
+        return redirect('dashboard');
+    }
+
+    public function update(Request $request){
+        $inputs = $request->all();
+        $keys = array_keys($request->all());
+
+        $produtosAtuais = $this->ultimaComanda($request->mesa_id);
+
+        for ($i = 2; $i < count($keys) - 2; $i++) {
+            $produto = $produtosAtuais[$i - 2];
+            $produto->quantidade = $inputs[$keys[$i]];
+            $produto->update();
+        }
+
+        return redirect('dashboard');
+    }
+
+    public function ultimaComanda($id){
+        $comandaid = Comanda::where('mesa_id', $id)->get();
+        $comandaid = $comandaid[count($comandaid) - 1]['id'];
+        $produtosAtuais = ProdutosComanda::where('comanda_id', $comandaid)->get();
+        return $produtosAtuais;
+    }
+
+}
